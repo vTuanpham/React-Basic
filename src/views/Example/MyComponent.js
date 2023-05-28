761738d74be731410aa25b2c667ae0de5445a2de
Tuan-Pham <weekend.2810@gmail.com>
2023-05-28 22:25:05 +0700

    chore: add docs, and log on handle and re-render

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -4,7 +4,7 @@ class MyComponent extends React.Component {
     // In a class component, you don't need to declare by let or const, react do it for you !
     // Object declare key:value
     state = {
-        name: 'Tuan',
+        name: '',
         git: 'vTuanPham',
 
     }
@@ -20,13 +20,18 @@ class MyComponent extends React.Component {
     // All handles must be arrow function as the old declare
     // using function must be bind to the class constructor
     handleOnChangeName = (event) => {
+        console.log('Event value: ', event.target.value, 'Event target: ', event.target, 'Event object: ', event)
+        // Merge
+        // setState update value of obj and callback react to re-render the page
         this.setState({
-            name: event.target.value
+            name: event.target.value,
         })
+
+        // Do not use this type of assign to change state 
+        // this.state.name = event.target.value;    // Bad code!
     }
 
     handleOnClick = () => {
-        console.log('You hit the button !')
         alert("You click me!")  // This handle doesn't use value from input, only activate or not so don't need to pass in event
     }
     // Note: In the JSX attr of the React Element, the function must also be an arrow function like the handle for it
@@ -34,6 +39,7 @@ class MyComponent extends React.Component {
     // Since this.state is an object you can call access it attr by either 
     // state.name or state['name']
     render() {
+        console.log(">>> Call render: ", this.state)    // This will be log everytime the handleOnChangeName is called since it automaticly call re-render
         return (
             <>
                 <div className='first'>
